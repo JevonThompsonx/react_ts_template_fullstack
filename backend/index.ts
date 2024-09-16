5240712c7674bf91e26c9b24bbf6dd2698a0e1c8
Jevon Thompson <104575457+JevonThompsonx@users.noreply.github.com>
2024-09-15 22:14:41 -0700

    Changed port according to documentation specifications

--- backend/index.ts
+++ backend/index.ts
@@ -6,11 +6,10 @@ app.get('/', (c) => {
 })
 
 const port = process.env.PORT || 8080
-Bun.serve({
-  fetch: app.fetch,
-  port: Number(port),
-  development: true,
-});
 
 console.log(`Listening on port: ${port}`)
-export default app
+
+export default { 
+  port: port, 
+  fetch: app.fetch, 
+} 
