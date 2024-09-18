4b0ec55cefb01979a0ccfc6ecd813e62390d12e4
Jevon Thompson <104575457+JevonThompsonx@users.noreply.github.com>
2024-09-17 23:55:07 -0700

    Cors

--- backend/index.ts
+++ backend/index.ts
@@ -1,7 +1,13 @@
 import { Hono } from 'hono'
+import { cors } from 'hono/cors';
 //import { serveStatic } from 'hono/bun'
 // for static type serving^
 const app = new Hono()
+
+app.use('*', cors({
+  origin: 'http://localhost:3000',  
+  allowMethods: ['GET', 'POST'],  
+}));
 
 // app.use('/static/*', serveStatic({ root: './' }))
 // ^ how you'd go about serving static files in a folder named 'static'
