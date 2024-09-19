849cb37b1e41283239f8b36853f530b348f83912
Jevon Thompson <104575457+JevonThompsonx@users.noreply.github.com>
2024-09-19 12:57:26 -0700

    changed hono to express

--- backend/index.ts
+++ backend/index.ts
@@ -1,26 +1,11 @@
-import { Hono } from 'hono'
-import { cors } from 'hono/cors';
-//import { serveStatic } from 'hono/bun'
-// for static type serving^
-const app = new Hono()
-
-app.use('*', cors({
-  origin: 'http://localhost:3000',  
-  allowMethods: ['GET', 'POST'],  
-}));
-
-// app.use('/static/*', serveStatic({ root: './' }))
-// ^ how you'd go about serving static files in a folder named 'static'
-
-app.get('/', (c) => {
-  return c.text("It's aliiiive!!")
+import express from 'express'
+const app = express()
+const port = process.env.PORT || 8080
+app.get('/', (req, res) => {
+  res.send('Hello')
 })
 
-const port = process.env.PORT || 8080
+app.listen(port, () => {
 
-console.log(`Listening on port: ${port}`)
-
-export default { 
-  port: port, 
-  fetch: app.fetch, 
-} 
+  console.log(`Listening on port: ${port}`)
+})
