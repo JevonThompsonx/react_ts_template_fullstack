6bc7518e462ef812c4607fc5ff26901ea46a3551
Jevon Thompson <104575457+JevonThompsonx@users.noreply.github.com>
2024-09-19 23:26:25 -0700

    removed express in favor of hono

--- backend/index.ts
+++ backend/index.ts
@@ -1,11 +1,20 @@
-import express from 'express'
-const app = express()
+import { Hono } from 'hono'
+import { cors } from 'hono/cors'
+const app = new Hono()
+
+// app.use('/static/*', serveStatic({ root: './' }))
+// ^ how you'd go about serving static files in a folder named 'static'
 const port = process.env.PORT || 8080
-app.get('/', (req, res) => {
-  res.send('Hello')
+app.use(cors())
+app.post('/api', async (c) => {
+  console.log('Request from backend')
+})
+Bun.serve({
+  port: port,
+  fetch: app.fetch
 })
 
-app.listen(port, () => {
+console.log(`Running on port ${port}`)
 
-  console.log(`Listening on port: ${port}`)
-})
+
+
